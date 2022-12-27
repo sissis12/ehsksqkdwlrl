0260d8845cf0f6e28cdd245c2ffb722ff2d263dc
sissis12 <115008914+sissis12@users.noreply.github.com>
2022-12-27 13:32:20 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,33 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+    if (input.lightLevel() > 5) {
+        basic.showLeds(`
+            # . . . #
+            . # # # .
+            . # . # .
+            . # # # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
